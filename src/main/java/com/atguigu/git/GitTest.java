fb054d0b18e0442d477156e28e53926b5474c651
baichuan <2986106320@qq.com>
2021-09-02 23:14:15 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author baichuan
+ * @date 2021/9/2 - 23:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
