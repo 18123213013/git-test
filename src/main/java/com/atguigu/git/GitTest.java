a6de24cbd2e1fd70c773620b19bb76014d59caf1
18123213013 <89542662+18123213013@users.noreply.github.com>
2021-09-02 23:48:09 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("master fix!");
         System.out.println("hot-fix!");
         System.out.println("push-fix!");
+        System.out.println("push-fix!");
     }
 }
