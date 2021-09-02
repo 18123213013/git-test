2927211e95fdef684011ed8873e8e07c89a58565
baichuan <2986106320@qq.com>
2021-09-02 23:45:00 +0800

    first commit3

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello git6!");
         System.out.println("master fix!");
         System.out.println("hot-fix!");
+        System.out.println("push-fix!");
     }
 }
